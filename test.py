bf37fb940aba7d4fad35aa9f8e5e9d7f2fd90a2d
sanghyunpark <48581113+tkdgus9910@users.noreply.github.com>
2023-04-04 17:58:35 +0900

    v0.2
    uploading sample data

--- test.py
+++ test.py
@@ -19,41 +19,29 @@ if __name__ == '__main__':
     import pickle
     from datetime import datetime
     from datetime import timezone
-
+    
     directory = os.path.dirname(os.path.abspath(__file__))
     directory = directory.replace("\\", "/") # window
-    os.chdir(directory)    
     sys.path.append(directory+'/submodule')
     
-    directory = 'D:/SNU/TILAB - 문서/DB/patent/wisdomain/blockchain/'
-    file_name = 'blockchain'
+    # load data
+    directory += '/input/'
+    file_name = 'sample'
     data = pd.read_csv(directory+ file_name+ '.csv', skiprows = 4)
-  
-    data = data.head(1000)
+    
+    
     data['file_name'] = file_name
+    
     
     #%% 1. 데이터 전처리    
     
     from preprocess import wisdomain_prep
     
-
     # from wisdomain import wisdomain_prep
     data_ = wisdomain_prep(data)    
     
-    #%% 2. 출원인 대표명화 - 작업중
-    import requests
-    import xmltodict
-    from requests.utils import quote
-    
-    applicant_rep = quote('INTEL CORP') # percent encoding
-    
-    url = 'https://assignment-api.uspto.gov/patent/lookup?query=' + applicant_rep
-    url += '&filter=OwnerName'
-    res = requests.get(url, verify=False)
-    
-    
-    result = xmltodict.parse(res.content)
-    #%% 3. 주체별 특허지표 계산
+
+    #%% 2. 주체별 특허지표 계산
     import indicator
     
     # 등록특허로 필터링
@@ -65,7 +53,7 @@ if __name__ == '__main__':
     pfs = indicator.calculate_PFS(data_registered, 'applicant_rep', 'family_INPADOC_country_count')
     
     
-    #%% 4. 분야 전체의 특성 계산
+    #%% 3. 분야 전체의 특성 계산
     
     
     cr4 = indicator.calculate_CRn(data_registered, 
@@ -81,4 +69,17 @@ if __name__ == '__main__':
     
     
     
+    #%% 4.  출원인 대표명화 - test
+    import requests
+    import xmltodict
+    from requests.utils import quote
+    
+    applicant_rep = quote('INTEL CORP') # percent encoding
+    
+    url = 'https://assignment-api.uspto.gov/patent/lookup?query=' + applicant_rep
+    url += '&filter=OwnerName'
+    res = requests.get(url, verify=False)
+    
+    
+    result = xmltodict.parse(res.content)
     
